38e7af18852f33050e73c3ec310f1d7c2b3866e7
itoroitich4-droid <itoroitich4@gmail.com>
2026-09-12 17:16:58 +0300

    edited register

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,54 +1,48 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
-    """A simple cash register model for adding items, applying discounts, and voiding transactions."""
-
     def __init__(self, discount=0):
-        self.discount = discount if self._is_valid_discount(discount) else 0
+        self._discount = 0
+        self.discount = discount
         self.total = 0
         self.items = []
         self.previous_transactions = []
 
-    def _is_valid_discount(self, discount):
-        if not isinstance(discount, int) or discount < 0 or discount > 100:
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, value):
+        if isinstance(value, int) and 0 <= value <= 100:
+            self._discount = value
+        else:
             print("Not valid discount")
-            return False
-        return True
 
-    def add_item(self, item, price, quantity=1):
-        transaction_amount = price * quantity
-        self.total += transaction_amount
+    def add_item(self, item, price, quantity):
+        self.total += price * quantity
 
-        for _ in range(quantity):
-            self.items.append(item)
+        self.items.append(item)
 
         self.previous_transactions.append({
             "item": item,
             "price": price,
-            "quantity": quantity,
+            "quantity": quantity
         })
 
     def apply_discount(self):
-        if not self.previous_transactions or self.discount == 0:
+        if not self.previous_transactions:
             print("There is no discount to apply.")
             return
 
-        discount_amount = self.total * self.discount / 100
-        self.total = self.total - discount_amount
-        print(f"After the discount, the total comes to ${int(self.total)}.")
+        self.total = self.total * (1 - self.discount / 100)
 
     def void_last_transaction(self):
         if not self.previous_transactions:
-            print("There is no transaction to void.")
             return
 
-        last_transaction = self.previous_transactions.pop()
-        self.total -= last_transaction["price"] * last_transaction["quantity"]
+        transaction = self.previous_transactions.pop()
 
-        for _ in range(last_transaction["quantity"]):
-            if last_transaction["item"] in self.items:
-                self.items.remove(last_transaction["item"])
+        self.total -= transaction["price"] * transaction["quantity"]
 
-        if self.total < 0:
-            self.total = 0
+        self.items.pop()
